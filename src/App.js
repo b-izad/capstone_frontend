011d5b8d2fb6bfce77782e78a005ad719194dd14
Bahareh <izad@bu.edu>
2022-08-09 12:55:05 -0400

    added to components for log in and sign up page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './form.js';
+import Login from './login.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+       
+        <div>
+          <Form/>
+          <Login/>
+        </div>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+    
+        </a> */}
       </header>
     </div>
   );
